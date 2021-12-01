9eb7d3ff9ba7df07a70f6cb956eb8f9303d81220
DevilJack <grigorysmorodnikov253@mail.ru>
2021-12-01 13:31:24 +0300

    cars

--- components/Train/TrainItem.js
+++ components/Train/TrainItem.js
@@ -1,15 +1,42 @@
 import st from "./TrainItem.module.css"
-import React from "react";
+import React, {useRef, useState} from "react";
 import cx from "classnames";
 import TicketInfo from "../Ticket/TicketInfo";
+import {convertDate} from "../../utils/utils";
+import DlTable from "../Shared/Table";
+import DlIcon from "../Shared/Icon";
 
+const carsHeaders = [
+    { name: "index", label: "№", width: "50px" },
+    { name: "number", label: "Номер вагона"},
+    { name: "type", label: "Тип вагона"},
+    { name: "amOfSeats", label: "Кол-во мест" },
+];
 
 const TrainItem = props => {
+
+    const onFooterClick = ev => {
+        ev.stopPropagation()
+    }
+
+    const footerContentRef = useRef(null)
+
+    const [isRoadCollapsed, setIsRoadCollapsed] = useState(true)
+
     const tag = {
         color: 'success',
         label: "Активен",
         dark: false,
     }
+
+    const carsTableData = props.cars.map((car, i) => {
+        return {
+            index: i + 1,
+            number: car.number,
+            type: car.type.name,
+            amOfSeats: car.type.amount_of_seats
+        }
+    })
 
     return (
         <div
@@ -22,6 +49,27 @@ const TrainItem = props => {
                         dateTitle={`Номер поезда ${props.train_id}`}
                         numberTitle={`Поезд ${props.name}`}
                     />
+                </div>
+            </div>
+            <div className={st.footer} onClick={onFooterClick}>
+                <div className={cx(st.footerContent, { [st.isCollapsed]: isRoadCollapsed })} ref={footerContentRef}>
+                    <div className={st.collapseSamples}>
+                        <div className={st.tableContainer}>
+                            <div className={st.bigger_subtitle}>Вагоны поезда</div>
+                            <div className={st.table}>
+                                <DlTable
+                                    headers={carsHeaders}
+                                    tableData={carsTableData}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={st.footerTitle} onClick={() => setIsRoadCollapsed(!isRoadCollapsed)}>
+                        <span className={cx(st.footerIcon, { [st.iconCollapsed]: isRoadCollapsed })}>
+                            <DlIcon name="keyboard_arrow_down" />
+                        </span>
+                    <span>{isRoadCollapsed ? 'Показать' : 'Скрыть'} подробности</span>
                 </div>
             </div>
         </div>
